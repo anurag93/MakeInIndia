d6d05b938d80a544991e96956f1f4c559acb9306
Anurag Sengupta <sengupta.anurag93@gmail.com>
2016-07-20 16:43:26 +0530

    Final commit 20/07/2016

--- php script/comments.php
+++ php script/comments.php
@@ -5,17 +5,17 @@
  define('DB','digital_india');
 
  $con=mysqli_connect(HOST,USER,PASS,DB) or die('Unable to connect');
- if($_SERVER['REQUEST_METHOD']==GET)
+ if($_SERVER['REQUEST_METHOD']=='GET')
  { 
  	//for entering the comments
-   if($_GET['edit_comment']==null )
+   if($_GET['edit_comment']=='0' )
    {
    	$username=$_GET['name'];
     $comments=$_GET['comments'];
     $city=$_GET['city'];
     $comment_id=$_GET['comment_id'];
 
-    $sql="INSERT INTO `comments`(`comment_id`,`username`, `comments`, `city`) VALUES ('$comment_id','$username','$comments','$city')";
+    $sql="INSERT INTO `comments`(`comment_id`,`username`, `comment`, `city`) VALUES ('$comment_id','$username','$comments','$city')";
     $result=mysqli_query($con,$sql);
    }
    //for updating the comments
@@ -23,10 +23,9 @@
    {
    	$username=$_GET['name'];
     $comments=$_GET['comments'];
-    $comment_id=$_GET['comment_id']
-    
+    $comment_id=$_GET['comment_id'];
 
-    $sql="UPDATE `comments` SET `comments`='$comments' WHERE `username`='$username' and `comment_id`='$comment_id'";
+    $sql="UPDATE `comments` SET `comment`='$comments' WHERE `comment_id`='$comment_id'";
     $result=mysqli_query($con,$sql);
    }
   
--- php script/get_comment.php
+++ php script/get_comment.php
@@ -1,1 +1,38 @@
-<? php
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('PASS','');
+define('DB','digital_india');
+
+$con=mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+if($_SERVER['REQUEST_METHOD']=='GET')
+{
+   if($_GET['all']!='0')
+   {
+   	
+   	$sql_comments="SELECT `comment_id`,`comment`,`threads`,`thread_id` FROM comments";
+    $result=mysqli_query($con,$sql_comments);
+    $tableArray=array();
+    while ($row=mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+    	$tableArray['AllComments']=$row;
+    }
+    echo json_encode($tableArray);
+    
+   }
+   else
+   {
+   	$city=$_GET['city'];
+   	$sql_comments="SELECT `comment_id`,`comment`,`threads`,`thread_id` FROM comments WHERE city='$city'";
+   	$result=mysqli_query($con,$sql_comments);
+   	$tableArray=array();
+   	while ($row=mysqli_fetch_assoc($result)) {
+   		$tableArray['comments']=$row;
+   	}
+   	echo json_encode($tableArray);
+   }
+}
+else
+{
+	echo "Error";
+}
+?>
